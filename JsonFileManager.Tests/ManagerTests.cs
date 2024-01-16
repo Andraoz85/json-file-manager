c9867bb13f454e32517780e46ef48385abe9513d
Andraoz85 <andraoz85@gmail.com>
2024-01-16 11:58:24 +0100

    Add two first tests

--- JsonFileManager.Tests/ManagerTests.cs
+++ JsonFileManager.Tests/ManagerTests.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
 
 namespace JsonFileManager.Tests
 {
-    internal class ManagerTests
+    public class ManagerTests
     {
+        // Har en dictionary, får JSON tillbaka
+        [Fact]
+        public void CreateJsonFromDict_ShouldReturnJson()
+        {
+            // Given
+            Dictionary<string, object> dict = new()
+            {
+                // { "key", "value" }
+                { "name", "Gandalf" },
+                { "Age", 3000 },
+                { "Profession", "Wizard" }
+            };
+            Manager manager = new();
+            // When
+            string expectedJson = JsonConvert.SerializeObject(dict);
+            string actualJson = manager.CreateJsonFromDict(dict);
+
+            // Then
+            actualJson.Should().NotBeNull();
+            actualJson.Should().BeEquivalentTo(expectedJson);
+        }
+
+        [Fact]
+
+        public void CreateJsonFromDict_EmptyDict_ShouldThrowArgumentException()
+        {
+            // Given
+            Dictionary<string, object> dict = new();
+            Manager manager = new();
+            // When
+            Action test = () => manager.CreateJsonFromDict(dict);
+
+            // Then
+            test.Should().Throw<ArgumentException>();
+        }
     }
 }
--- JsonFileManager/Manager.cs
+++ JsonFileManager/Manager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace JsonFileManager
 {
@@ -10,11 +6,19 @@ namespace JsonFileManager
     {
         // Json till Dictionary
 
-
         // Dictionary till Json
+        public string CreateJsonFromDict(Dictionary<string, object> dict)
+        {
+            if (!dict.Any())
+                throw new ArgumentException();
+
+            return JsonConvert.SerializeObject(dict);
+        }
+
 
         // Läsa Json-fil
 
         // Skriva Json-fil
+
     }
 }
