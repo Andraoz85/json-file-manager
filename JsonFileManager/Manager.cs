b4d8004552b342177f624c0d9bc2ef9500af596c
Andraoz85 <andraoz85@gmail.com>
2024-01-16 10:10:30 +0100

    Initial commit

---
+++ JsonFileManager/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFileManager
+{
+    public class Manager
+    {
+        // Json till Dictionary
+
+
+        // Dictionary till Json
+
+        // Läsa Json-fil
+
+        // Skriva Json-fil
+    }
+}
